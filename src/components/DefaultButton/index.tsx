bacd4deee7b78af7ed73c94921a9cbe5555a8441
Hamilton Gonçalves <hamiltongsj@gmail.com>
2026-01-31 20:23:43 -0300

    Creating a reusable button component with options

---
+++ src/components/DefaultButton/index.tsx
@@ -0,0 +1,24 @@
+import type React from 'react';
+
+import styles from './styles.module.css';
+
+type DefaultButtonProps = {
+  icon: React.ReactNode;
+  color?: 'green' | 'red';
+} & React.ComponentProps<'button'>;
+
+function DefaultButton({
+  icon,
+  color = 'green',
+  ...props
+}: DefaultButtonProps) {
+  return (
+    <>
+      <button className={`${styles.button} ${styles[color]}`} {...props}>
+        {icon}
+      </button>
+    </>
+  );
+}
+
+export { DefaultButton };
