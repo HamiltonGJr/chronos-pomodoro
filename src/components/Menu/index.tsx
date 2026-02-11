ded2b1c5ba9a4dc2a45db6fa35e6f40c7a0242ea
Hamilton Gonçalves <hamiltongsj@gmail.com>
2026-02-11 20:15:33 -0300

    Capturing link click event with preventDefault for dark and light theme

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,24 +1,54 @@
+import { useState } from 'react';
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
 
 import styles from './styles.module.css';
 
+type AvailableThemes = 'dark' | 'light';
+
 function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>('dark');
+
+  function handleThemeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) {
+    event.preventDefault();
+
+    console.log('Clicando', Date.now());
+  }
+
   return (
     <>
       <nav className={`${styles.menu} ${styles.cyan}`}>
-        <a className={styles.menuLink} href='#'>
+        <h1>{theme}</h1>
+        <a className={styles.menuLink} href='#' aria-label='Home' title='Home'>
           <HouseIcon />
         </a>
 
-        <a className={styles.menuLink} href='#'>
+        <a
+          className={styles.menuLink}
+          href='#'
+          aria-label='Histórico'
+          title='Histórico'
+        >
           <HistoryIcon />
         </a>
 
-        <a className={styles.menuLink} href='#'>
+        <a
+          className={styles.menuLink}
+          href='#'
+          aria-label='Configurações'
+          title='Configurações'
+        >
           <SettingsIcon />
         </a>
 
-        <a className={styles.menuLink} href='#'>
+        <a
+          className={styles.menuLink}
+          href='#'
+          aria-label='Tema: Claro | Escuro'
+          title='Tema: Claro | Escuro'
+          onClick={handleThemeChange}
+        >
           <SunIcon />
         </a>
       </nav>
